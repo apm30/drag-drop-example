9f29ae809359cb445f4af4ba970deb1d7a36e346
Domo <stolarczyk.dominik@gmail.com>
2019-03-27 11:29:27 +0100

    +second list, css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -21,4 +26,26 @@ export class AppComponent {
     'Artist XII - Seyi Shay',
     'Artist XIII - Teni'
   ];
+
+  alteArtists = [
+    'Artist 1 - Odunsi',
+    'Artist 2 - Nonso',
+    'Artist 3 - Wavy the creator',
+    'Artist 4 - Dwin',
+    'Artist 5 - SDC',
+    'Artist 6 - Teni'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer !== event.container) {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      moveItemInArray(this.artists, event.previousIndex, event.currentIndex);
+    }
+  }
 }
